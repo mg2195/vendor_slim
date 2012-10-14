f8a1f602d0a4119f4a5b3dd3d3fc479576f5375c
mg2195 <mikeyg2195@gmail.com>
2012-10-14 13:27:56 -0700

    Add infuse4g to build list

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,5 +13,5 @@ add_lunch_combo slim_i777-userdebug
 add_lunch_combo slim_n7000-userdebug
 add_lunch_combo slim_endeavoru-userdebug
 add_lunch_combo slim_vibrantmtd-userdebug
-
+add_lunch_combo slim_infuse4g-userdebug
 
